79ae165762eaa1825137ac57946a2093aa06ed75
ziad729 <55300514+ziad729@users.noreply.github.com>
2021-12-28 12:54:29 +0200

    control unit started

--- Components/CTRL_UNIT.v
+++ Components/CTRL_UNIT.v
@@ -2,24 +2,17 @@ module CTRL_UNIT(
  clk,
  opcode,
  reset,
- operation,
- en32,
- we, 
- re
+ signals
  );
 
 input clk;
 input [6:0] opcode;
 input reset;
-output [2:0] operation;
-output en32;
-output we;
-output re;
+output [2:0] signals;
 
 always @(posedge clk)
 begin: ctrl_unit_op
-    case (operation)  
-        
-    endcase
+    
 end // ctrl_unit_op
 endmodule // CTRL_UNIT
+//{PCselect,PCplace,,write data sel,addres src,Mem Read,Mem write,en32,mem buffer en,Reg Wite,WB src}
