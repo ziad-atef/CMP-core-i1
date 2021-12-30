f3fac4cc6953cda619d445b85ed0bbfcbcf01ca3
ziad729 <55300514+ziad729@users.noreply.github.com>
2021-12-30 16:42:59 +0200

    Control unit editted

--- Components/CTRL_UNIT.v
+++ Components/CTRL_UNIT.v
@@ -8,11 +8,17 @@ module CTRL_UNIT(
 input clk;
 input [6:0] opcode;
 input reset;
-output [2:0] signals;
+output reg [22:0]signals;
+
 
 always @(posedge clk)
 begin: ctrl_unit_op
-    
+        case (opcode)  
+                7'b0010001 :    signals <= 23'b01110100000101001100011;
+                7'b0000011 :    signals <= 23'b01110101000100001100011;
+                7'b0011001 :    signals <= 23'b01100100000101011100011;
+                7'b0011000 :    signals <= 23'b01111100000101011100011;
+        endcase 
 end // ctrl_unit_op
 endmodule // CTRL_UNIT
 //{PCselect,PCplace,,write data sel,addres src,Mem Read,Mem write,en32,mem buffer en,Reg Wite,WB src}
