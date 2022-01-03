1a3c9391c754caaec1373bc9c06308d010f57bee
omar214 <omarmohamed2322000@gmail.com>
2022-01-03 02:18:22 +0200

    done Decode ✔

--- Components/CTRL_UNIT.v
+++ Components/CTRL_UNIT.v
@@ -1,24 +1,23 @@
 module CTRL_UNIT(
- clk,
- opcode,
- reset,
- signals
+        clk,
+        opcode,
+        reset,
+        signals
  );
 
-input clk;
-input [6:0] opcode;
-input reset;
-output reg [22:0]signals;
+        input clk;
+        input [6:0] opcode;
+        input reset;
+        output reg [22:0]signals;
 
 
-always @(posedge clk)
-begin: ctrl_unit_op
-        case (opcode)  
-                7'b0010001 :    signals <= 23'b01110100000101001100011;
-                7'b0000011 :    signals <= 23'b01110101000100001100011;
-                7'b0011001 :    signals <= 23'b01100100000101011100011;
-                7'b0011000 :    signals <= 23'b01111100000101011100011;
-        endcase 
-end // ctrl_unit_op
+        always @(posedge clk)
+        begin: ctrl_unit_op
+                case (opcode)  
+                        7'b0010001 :    signals = 23'b01110100000101001100011;
+                        7'b0000011 :    signals = 23'b01110101000100001100011;
+                        7'b0011001 :    signals = 23'b01100100000101011100011;
+                        7'b0011000 :    signals = 23'b01111100000101011100011;
+                endcase 
+        end // ctrl_unit_op
 endmodule // CTRL_UNIT
-//{PCselect,PCplace,,write data sel,addres src,Mem Read,Mem write,en32,mem buffer en,Reg Wite,WB src}
---
+++ stages/Decode/Decode.v
@@ -0,0 +1,38 @@
+module decode(
+    clk,
+    rst,
+    opcode,
+    regWrite,
+    Rsrc1,
+    Rsrc2,
+    Rdst,
+    writeData,
+    inPort,
+    signals,
+    readData1,
+    readData2,
+);
+    input clk;
+    input rst;
+    input regWrite;
+    input [2:0] Rsrc1;
+    input [2:0] Rsrc2;
+    input [2:0] Rdst;
+    input [6:0] opcode;
+    input [15:0] writeData;
+    input [15:0] inPort;
+    output [22:0] signals;
+    output reg [15:0] readData1;
+    output [15:0] readData2;
+        
+    wire [15:0] regFileOut;
+    CTRL_UNIT u0(.clk(clk),.opcode(opcode),.reset(rst),.signals(signals));
+    reg_file  u1(.clk(clk),.regWrite(regWrite),.writeData(writeData),.Rsrc1(Rsrc1),.Rsrc2(Rsrc2),.Rdst(Rdst),.readData1(regFileOut),.readData2(readData2));
+    always @(posedge clk) 
+    begin: decode_unit_op
+        if(signals[18]) //  detect if output is from in Register or register file
+            readData1 <= inPort;
+        else
+            readData1 <= regFileOut;
+    end// decode_unit_op
+endmodule
