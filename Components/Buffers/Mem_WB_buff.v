c59e8acc5982ee2bc245128008376fcaa1ff9f31
omar214 <omarmohamed2322000@gmail.com>
2022-01-03 02:17:34 +0200

    remove reset from  buffers

--- Components/Buffers/Mem_WB_buff.v
+++ Components/Buffers/Mem_WB_buff.v
@@ -1,28 +1,33 @@
-module Mem_WB_buff (
-    input  clk,rst,enable,
+module Mem_WB_buff     
+    #( parameter    
+        WbSize  = 4
+    )(
+    // input rst, 
+    input  clk,enable,
 
-    input        i_WB;
-    input [31:0] i_Mem ;
-    input [15:0] i_alu;
-    input [2:0]  i_Rdst; 
+    input  [WbSize-1 :0 ] i_WB,
+    input  [31:0] i_MemData ,
+    input  [15:0] i_alu,
+    input  [2:0 ] i_Rdst, 
 
-    output reg        o_WB;
-    output reg [31:0] o_Mem ;
-    output reg [15:0] o_alu;
-    output reg [ 2:0] o_Rdst; 
+    output [2: 0] o_WB,
+    output [31:0] o_MemData ,
+    output [15:0] o_alu,
+    output [ 2:0] o_Rdst 
 );
-    always @(posedge clk) begin
-        if(rst == 1'b1) begin
-            o_WB  <=       0;
-            o_Mem <=       0;
-            o_alu <=       0;
-            o_Rdst<=       0;
-        end
-        else if(enable == 1'b1) begin
-            o_WB  <=       i_WB;
-            o_Mem <=       i_Mem;
-            o_alu <=       i_alu;
-            o_Rdst<=       i_Rdst;
+    always @(negedge clk) begin
+        // if(rst == 1'b1) begin
+        //     o_WB  <=       0;
+        //     o_Mem <=       0;
+        //     o_alu <=       0;
+        //     o_Rdst<=       0;
+        // end
+        // else 
+        if(enable == 1'b1) begin
+            o_WB       <=  i_WB;
+            o_MemData  <=  i_MemData;
+            o_alu      <=  i_alu;
+            o_Rdst     <=  i_Rdst;
         end
     end
 endmodule
--- Components/Buffers/dec_ALU_buff.v
+++ Components/Buffers/dec_ALU_buff.v
@@ -1,30 +1,43 @@
-module dec_alu_buf (
-    input  clk,rst,enable,
+    #( parameter    
+        WbSize  = 4,
+        MemSize = 6,
+        ExSize  = 3
+    )
+module dec_alu_buf 
+    (
+    // input rst,
+    input  clk,enable,
 
-    input i_WB, i_Mem, i_Ex , i_chg_flag;
-    input [31:0] i_pc   ;
-    input [2:0]  i_Rsrc1 , i_Rsrc2 , i_Rdst;
-    input [15:0] i_immd, i_read_data1, i_read_data2;
+    input [WbSize -1 :0] i_WB, 
+    input [MemSize-1 :0] i_Mem, 
+    input [ExSize-1  :0] i_Ex , 
+    input i_chg_flag,
+    input [31:0] i_pc,
+    input [2: 0] i_Rsrc1 , i_Rsrc2 , i_Rdst,
+    input [15:0] i_immd, i_read_data1, i_read_data2,
 
-    output reg o_WB, o_Mem, o_Ex , o_chg_flag;
-    output reg [31:0] o_pc   ;
-    output reg [2:0]  o_Rsrc1 , o_Rsrc2 , o_Rdst;
-    output reg [15:0] o_immd , o_read_data1, o_read_data2;
+    output [WbSize -1 :0] o_WB, 
+    output [MemSize-1 :0] o_Mem, 
+    output [ExSize-1  :0] o_Ex , 
+    output o_chg_flag,
+    output [31:0] o_pc   ,
+    output [2:0]  o_Rsrc1 , o_Rsrc2 , o_Rdst,
+    output [15:0] o_immd , o_read_data1, o_read_data2
 );
     always @(posedge clk) begin
-        if(rst == 1'b1) begin
-            o_WB <=        0;
-            o_Mem <=       0;
-            o_Ex <=        0;
-            o_chg_flag <=  0;
-            o_pc<=         0;
-            o_read_data1<= 0;
-            o_read_data2<= 0;
-            o_Rsrc1<=      0;
-            o_Rsrc2<=      0;
-            o_Rdst<=       0;
-            o_immd<=       0;
-        end
+        // if(rst == 1'b1) begin
+        //     o_WB <=        0;
+        //     o_Mem <=       0;
+        //     o_Ex <=        0;
+        //     o_chg_flag <=  0;
+        //     o_pc<=         0;
+        //     o_read_data1<= 0;
+        //     o_read_data2<= 0;
+        //     o_Rsrc1<=      0;
+        //     o_Rsrc2<=      0;
+        //     o_Rdst<=       0;
+        //     o_immd<=       0;
+        // end
         else if(enable == 1'b1) begin
             o_WB <=         i_WB;
             o_Mem <=        i_Mem;
