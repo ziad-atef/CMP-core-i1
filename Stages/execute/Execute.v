4cb16a4b94e349b004f72b46ea5c0e824c9ca568
ziad729 <55300514+ziad729@users.noreply.github.com>
2022-01-07 20:53:22 +0200

    Revert "Finish execute with testbench"
    This reverts commit 5ed35aed3ddfbcdf82042aa50ffb8b30a59660df.

--- Stages/execute/Execute.v
+++ Stages/execute/Execute.v
@@ -1,19 +1,16 @@
 module execute(
     clk,
-    flag_src,
     data1,
     data2,
     imm,
     ALUsrc1,
     ALUsrc2,
     ALUoperation,
-    mem_flags,
-    input_flags,
-    imm_val,
+    flag_src,
     data1_val,
     data2_val,
-    prev_ALU,
-    prev_mem,
+    imm_val,
+    input_flags,
     ALU_out,
     output_flags
 );
@@ -22,48 +19,41 @@ module execute(
     input data1;
     input data2;
     input imm;
-    input [1:0]  ALUsrc1;
-    input [1:0]  ALUsrc2;
-    input [2:0]  ALUoperation;
-    input [3:0]  mem_flags;
-    input [3:0]  input_flags;
+    input [1:0]ALUsrc1;
+    input [1:0]ALUsrc2;
+    input [2:0]ALUoperation;
+    input [3:0]input_flags;
     input [15:0] imm_val;
     input [15:0] data1_val;
     input [15:0] data2_val;
-    input [15:0] prev_ALU;
-    input [15:0] prev_mem;
-
+    
     output [15:0] ALU_out;
     output [3:0] output_flags;
 
 
-    wire  [3:0] ALU_output_flags; 
-    wire [15:0]data1_mux,data2_mux,imm_mux; 
-    reg [15:0] ALU_input2;
-    reg [15:0] ALU_input1;
-
+    wire [15:0]data1_mux,data2_mux,imm_mux;
 
     assign data1_mux = (data1)?data1_val:data2_val;
     assign data2_mux = (data2)?data2_val:1;
     assign imm_mux = (imm)?imm_val:data2_mux;
-    assign output_flags = (flag_src)?mem_flags:ALU_output_flags;
 
-
-    ALU u0(.operand1(ALU_input1), .operand2(ALU_input2), .operation(ALUoperation), .flags_in(input_flags), .result(ALU_out), .flags_out(ALU_output_flags));
+    ALU u0(.operand1(data1_mux), .operand2(imm_mux), .operation(ALUoperation), .flags_in(4'b0), .result(ALU_out), .flags_out(flags));
     
-    always @(*) begin
-        case(ALUsrc1)
-            2'b00: ALU_input1 = data1_mux;
-            2'b01 : ALU_input1 = prev_ALU;
-            2'b10: ALU_input1 = prev_mem;
-            default : ALU_input1 = data1_mux;            
-        endcase
-        case(ALUsrc2)
-            2'b00: ALU_input2 = data2_mux;
-            2'b01 : ALU_input2 = prev_ALU;
-            2'b10: ALU_input2 = prev_mem;
-            default : ALU_input2 = data2_mux;            
-        endcase
-    end
-
+    
+    /*
+    always @(posedge clk) 
+    begin: execute_unit_op
+        
+    end// execute_unit_op
+    
+    CTRL_UNIT u0(.clk(clk),.opcode(opcode),.reset(rst),.signals(signals));
+    reg_file  u1(.clk(clk),.regWrite(regWrite),.writeData(writeData),.Rsrc1(Rsrc1),.Rsrc2(Rsrc2),.Rdst(Rdst),.readData1(regFileOut),.readData2(readData2));
+    always @(posedge clk) 
+    begin: decode_unit_op
+        if(signals[18]) //  detect if output is from in Register or register file
+            readData1 <= inPort;
+        else
+            readData1 <= regFileOut;
+    end// decode_unit_op
+    */
 endmodule
